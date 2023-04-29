03a464265d148df9453471fab0cd6eb6a31c92c2
neneeye <neneeye@gmail.com>
2023-04-29 19:45:53 +0000

    new

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starts a Flask Web Application
 """
 
 from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def index():
